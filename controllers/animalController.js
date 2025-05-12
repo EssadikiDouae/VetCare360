7557dbc93f0f809c60feb13a146d8d8e583d6f9b
EssadikiDouae <DOUAE.ESSADIKI.ESTN24@ump.ac.ma>
2025-05-12 10:01:02 +0100

    Update animalController.js

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,34 +1,12 @@
-/*const Animal = require('../models/Animal');
-
-// Ajouter un nouvel animal
-exports.addAnimal = async (req, res) => {
-  try {
-    const newAnimal = new Animal(req.body);
-    const savedAnimal = await newAnimal.save();
-    res.status(201).json(savedAnimal);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-// Obtenir tous les animaux
-exports.getAnimals = async (req, res) => {
-  try {
-    const animals = await Animal.find().populate('proprietaire');
-    res.status(200).json(animals);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-*/const Animal = require('../models/Animal');
+const Animal = require('../models/Animal');
 const Proprietaire = require('../models/Proprietaire');
 
-// دالة لإضافة حيوان جديد
+
 const addAnimal = async (req, res) => {
   try {
     const { name, birthDate, type, owner } = req.body;
 
-    // إنشاء حيوان جديد
+    
     const newAnimal = new Animal({
       name,
       birthDate,
@@ -36,10 +14,10 @@ const addAnimal = async (req, res) => {
       owner,
     });
 
-    // حفظ الحيوان في قاعدة البيانات
+    
     await newAnimal.save();
 
-    // ✅ إضافة الحيوان إلى قائمة animaux الخاصة بالمالك
+    
     const proprietaire = await Proprietaire.findById(owner);
     if (proprietaire) {
       proprietaire.animaux.push(newAnimal._id);
@@ -47,7 +25,7 @@ const addAnimal = async (req, res) => {
       console.log(`✔️ Animal ajouté et lié à propriétaire: ${proprietaire.nom}`);
     }
 
-    // إرجاع الحيوان الذي تم إضافته
+   
     res.status(201).json(newAnimal);
   } catch (error) {
     console.error('Erreur lors de l\'ajout de l\'animal:', error);
@@ -55,12 +33,12 @@ const addAnimal = async (req, res) => {
   }
 };
 
-// دالة للحصول على حيوان بواسطة معرّفه
+
 const getAnimalById = async (req, res) => {
   try {
     const animal = await Animal.findById(req.params.id)
-      .populate('owner') // لجلب معلومات المالك
-      .populate('visites'); // لجلب الزيارات المتعلقة بالحيوان
+      .populate('owner') 
+      .populate('visites'); 
 
     if (!animal) {
       return res.status(404).json({ message: 'Animal non trouvé' });
